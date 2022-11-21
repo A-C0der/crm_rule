7d050ede8cea80c6a149b213ffc9c01f859dcdb2
A-C0der <106011669+A-C0der@users.noreply.github.com>
2022-11-21 20:43:28 +1030

    Add files via upload

---
+++ crm_rule/__manifest__.py
@@ -0,0 +1,13 @@
+{
+	'name':"crm rules",
+	'version':'1.0',
+	'author' : 'Htet Arkar Kyaw',
+	'description': """  
+                CRM Login User        
+    """,
+	'application':True,
+	'data':[
+        'security/crm_rule.xml',
+
+	],
+}
